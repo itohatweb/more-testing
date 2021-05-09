d302ec0de777aa9bbdc7590254c0f51f25983717
ITOH <to@itoh.at>
2021-05-09 16:14:29 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ const Permissions = require("../constants/permissions.js");
 const router = require("express").Router();
 const fetch = require("node-fetch");
 const http = require("http");
+const { execFile } = require("child_process");
 
 const express = require("express");
 const app = express();
@@ -15,12 +16,24 @@ router.post("/container/:id/:option", async (req, res) => {
     if (!valid) return res.status(401).json({ error: "Invalid token provided" });
 
     // curl --unix-socket /var/run/docker.sock -X POST http://localhost/v1.41/containers/0c79f2c7c52d4efc10877469172c3288b5278850720e97193d8bc9a376774715/stop
-    const agent = new http.Agent();
-    agent.createConnection({ path: "/var/run/docker.sock" });
-    await fetch(`http://host.docker.internal/v1.41/containers/${req.params.id}/${req.params.option}`, {
-      agent,
-      method: "POST",
-    }).then(console.log, console.error);
+    // const agent = new http.Agent();
+    // agent.createConnection({ path: "/var/run/docker.sock" });
+    // await fetch(`http://host.docker.internal/v1.41/containers/${req.params.id}/${req.params.option}`, {
+    //   agent,
+    //   method: "POST",
+    // }).then(console.log, console.error);
+
+    const child = await execFile(
+      "/usr/bin/curl",
+      ["--unix-socket /var/run/docker.sock", "-X GET", "http://localhost/v1.41/containers/json"],
+      { shell: "/bin/bash" },
+      function (error, stdout, stderr) {
+        if (error) {
+          throw error;
+        }
+        console.log(stdout);
+      }
+    );
 
     // 2021-05-08T20:07:52.757182957Z
 
