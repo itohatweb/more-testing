6e6e4053c8fb4b5eba18c08d70319acbb95ba77d
ITOH <to@itoh.at>
2021-05-09 13:59:06 +0200

    fix: use env variable for port

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const connection = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST,
-  port: 30449,
+  port: process.env.DB_PORT,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
